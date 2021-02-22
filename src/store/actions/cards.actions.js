04e015e781312d9caf46973d2e356f0ae7720be2
RomanLobai2021 <r.lobai@relevant.software>
2021-02-22 14:26:18 +0200

    Swipe card functionality

--- src/store/actions/cards.actions.js
+++ src/store/actions/cards.actions.js
@@ -1,5 +1,7 @@
 import { createRequestTypes, createActionTypes } from "utils/redux";
 
 export const GET_CARDS = createRequestTypes("GET_CARDS");
+export const SWIPE_CARD = createRequestTypes("SWIPE_CARD");
 
 export const getCards = createActionTypes(GET_CARDS);
+export const swipeCard = createActionTypes(SWIPE_CARD);
